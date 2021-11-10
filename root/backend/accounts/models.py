b4113db81c0b0497d4af8f94b451db074f62d0d8
jorge-cobos <cobos1932@gmail.com>
2021-11-10 14:44:46 -0600

    finished djangoRF custom user and auth

---
+++ root/backend/accounts/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+import uuid
+# Create your models here.
+class UserAccountManager(BaseUserManager):
+    def create_user(self, username, name, password=None):
+        user = self.model(username=username, name=name)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(
+        primary_key = True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    username = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.username
